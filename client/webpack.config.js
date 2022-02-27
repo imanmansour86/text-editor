c448648405b6e0b48e855d9d7718870ce5f2878e
imanmansour86 <imanmansour86@gmail.com>
2022-02-26 21:29:33 -0800

    adding webpack manifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
@@ -18,9 +19,31 @@ module.exports = () => {
       path: path.resolve(__dirname, "dist"),
     },
     plugins: [
+      new GenerateSW(),
       new HtmlWebpackPlugin({
         template: "./index.html",
         title: "Webpack Plugin",
+      }),
+      new WorkboxPlugin.GenerateSW(),
+      new InjectManifest({
+        swSrc: "./src/sw.js",
+        swDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Jate",
+        short_name: "jate",
+        description: "Keep track of important tasks!",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("assets/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
       }),
     ],
 
@@ -37,6 +60,10 @@ module.exports = () => {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
